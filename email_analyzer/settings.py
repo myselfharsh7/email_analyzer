e45c3f8791f13b5dbe9c8cb694c1593d2c7be28b
Harsh Kumar Gupta <114075413+myselfharsh7@users.noreply.github.com>
2025-03-03 21:54:14 +0530

    Update settings.py

--- email_analyzer/settings.py
+++ email_analyzer/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 from pathlib import Path
